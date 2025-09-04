d0407fd1842e1e76178bf27e96f501299ad34943
kang-jaehyun <jaehyunkang@yonsei.ac.kr>
2025-09-05 08:38:27 +0900

    add dataconfig for 44dim joint actions

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -1906,6 +1906,100 @@ class allex_theone_bimanual_full_long_config(BaseDataConfig):
 ###########################################################################################
 
 ###########################################################################################
+class allex_theone_bimanual_joint_config(BaseDataConfig):
+    video_keys = ["video.camera_agentview"]
+    state_keys = [
+        "state.right_arm_joints",
+        "state.left_arm_joints",
+        "state.right_hand_joints",
+        "state.left_hand_joints",
+    ]
+    action_keys = [
+        "action.right_arm_eef_pos",
+        "action.left_arm_eef_pos",
+        "action.right_finger_joints",
+        "action.left_finger_joints",
+    ]
+    language_keys = ["annotation.human.task_description"]
+    observation_indices = [0]
+    action_indices = list(range(32))
+    action_dim = 44
+
+    def modality_config(self) -> dict[str, ModalityConfig]:
+        video_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.video_keys,
+        )
+
+        state_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.state_keys,
+        )
+
+        action_modality = ModalityConfig(
+            delta_indices=self.action_indices,
+            modality_keys=self.action_keys,
+        )
+
+        language_modality = ModalityConfig(
+            delta_indices=self.observation_indices,
+            modality_keys=self.language_keys,
+        )
+
+        modality_configs = {
+            "video": video_modality,
+            "state": state_modality,
+            "action": action_modality,
+            "language": language_modality,
+        }
+
+        return modality_configs
+
+    def transform(self) -> ModalityTransform:
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={key: "min_max" for key in self.state_keys},
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={key: "min_max" for key in self.action_keys},
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            # model-specific transform
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=self.action_dim,
+                
+            ),
+        ]
+        return ComposedModalityTransform(transforms=transforms)
+###########################################################################################
+
+###########################################################################################
 class pos_only_right_arm_robotview_config(BaseDataConfig):
     video_keys = ["video.camera_ego"]
     state_keys = [
@@ -2413,4 +2507,6 @@ DATA_CONFIG_MAP = {
     "allex_theone_bimanual_long": allex_theone_bimanual_long_config(),
     "allex_theone_bimanual_full": allex_theone_bimanual_full_config(),
     "allex_theone_bimanual_full_long": allex_theone_bimanual_full_long_config(),
+
+    "allex_theone_bimanual_joint": allex_theone_bimanual_joint_config(),
 }
