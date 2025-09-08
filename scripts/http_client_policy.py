5cb2fb26889a5f8497620af10b896583d2480a83
youliangt <youliangt@nvidia.com>
2025-09-07 22:11:08 -0700

    provide example rtc script
    Signed-off-by: youliangt <youliangt@nvidia.com>

---
+++ scripts/http_client_policy.py
@@ -0,0 +1,165 @@
+"""
+This is an example of how to use the HttpClientPolicy to get the action from the robot.
+
+pip install requests json-numpy
+"""
+
+import time
+from collections import deque
+from typing import Any
+
+import numpy as np
+
+try:
+    import json_numpy
+    import numpy as np
+    import requests
+
+    json_numpy.patch()
+except ImportError:
+    print("json_numpy or requests not found")
+    pass
+
+
+class HttpClientPolicy:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+
+    def get_action(
+        self, observation: dict[str, Any], config: dict[str, Any] = None
+    ) -> dict[str, Any]:
+        print(f" -> GET ACTION CONFIG: {config}")
+        response = requests.post(
+            f"http://{self.host}:{self.port}/act",
+            json={"observation": observation, "config": config},
+        )
+        return response.json()
+
+
+class RTCPolicyWrapper:
+    """
+    This wraps the vanilla policy.get_action(obs) with a realtime chunking policy wrapper
+
+    control_freq: the control frequency of the robot, where number of steps in 1 second
+    denoising_steps: the number of steps to denoise the action
+    max_rtc_overlap_factor: the maximum overlap factor for the RTC policy,
+        0 to 1, where 1 means take entire overlap chunk, 0 means no overlap
+    """
+
+    def __init__(
+        self,
+        policy: HttpClientPolicy,
+        control_freq: int,
+        denoising_steps: int = 8,
+        max_rtc_overlap_factor: float = 0.75,
+    ):
+        self.policy = policy
+        self.control_freq = control_freq
+        self.latency_queue = deque(maxlen=10)
+        self.latency_queue.append(0)  # initialize with 0
+        self.denoising_steps = denoising_steps
+        self._previous_action = None
+        self._last_inference_completion_time = 0
+        self._action_horizon = None
+        self._max_rtc_overlap_factor = max_rtc_overlap_factor
+
+    def get_action(self, observation: dict[str, Any]) -> dict[str, Any]:
+        if self._previous_action is not None:
+            observation = {**observation, **self._previous_action}
+            config = self._get_config()
+        else:
+            config = None
+        start_time = time.monotonic()
+        action = self.policy.get_action(observation, config)
+        self._previous_action = action
+
+        # get the action horizon
+        if self._action_horizon is None:
+            self._set_action_horizon(action)
+
+        # update latency
+        latency = time.monotonic() - start_time
+        if latency > (1 / self.control_freq) * self._action_horizon:
+            print(f"Latency is too high: {latency} seconds, skipping")
+        else:
+            self.latency_queue.append(latency)
+
+        self._last_inference_completion_time = time.monotonic()
+        return action
+
+    def _set_action_horizon(self, action: dict[str, Any]):
+        # get the first key of the action, and get the shape of the value
+        for k in action.keys():
+            # check if it is a numpy array
+            if isinstance(action[k], np.ndarray):
+                self._action_horizon = action[k].shape[0]
+                print(f"action_horizon: {self._action_horizon}")
+                return
+        raise ValueError("No numpy array found in action")
+
+    def _get_config(self) -> dict[str, Any]:
+        # get avg latency
+        assert self._action_horizon is not None, "action_horizon is not set"
+        avg_latency = sum(self.latency_queue) / len(self.latency_queue)
+        frozen_steps = int(avg_latency * self.control_freq)
+
+        between_inference_time = time.monotonic() - self._last_inference_completion_time
+        executed_steps = int(self.control_freq * between_inference_time)
+        max_rtc_steps = self._action_horizon * self._max_rtc_overlap_factor
+
+        frozen_steps = max(min(frozen_steps, max_rtc_steps), 0)
+        overlap_steps = max(
+            min(self._action_horizon - executed_steps + frozen_steps, max_rtc_steps), frozen_steps
+        )
+
+        assert (
+            self._action_horizon >= overlap_steps >= 0
+        ), f"overlap_steps is not within bounds, {overlap_steps}"
+        assert (
+            self._action_horizon >= frozen_steps >= 0
+        ), f"rtc_frozen_steps is not within bounds, {frozen_steps}"
+
+        return {
+            "denoising_steps": self.denoising_steps,
+            "rtc_overlap_steps": overlap_steps,
+            "rtc_frozen_steps": frozen_steps,
+        }
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=5555)
+    parser.add_argument("--control_freq", type=int, default=20)
+    parser.add_argument("--denoising_steps", type=int, default=4)
+    parser.add_argument("--max_rtc_overlap_factor", type=float, default=0.75)
+    args = parser.parse_args()
+
+    policy = HttpClientPolicy(args.host, args.port)
+    policy = RTCPolicyWrapper(
+        policy, args.control_freq, args.denoising_steps, args.max_rtc_overlap_factor
+    )
+
+    for i in range(20):
+        t = time.time()
+        obs = {
+            "video.test": np.zeros((1, 480, 640, 3), dtype=np.uint8),
+            "video.ego_view": np.zeros((1, 256, 256, 3), dtype=np.uint8),
+            "state.left_arm": np.random.rand(1, 7),
+            "state.right_arm": np.random.rand(1, 7),
+            "state.left_hand": np.random.rand(1, 6),
+            "state.right_hand": np.random.rand(1, 6),
+            "state.waist": np.random.rand(1, 3),
+            "annotation.human.action.task_description": ["do your thing!"],
+        }
+        action = policy.get_action(obs)
+
+        # NOTE(youliang): you will execute your action here in the real robot
+        # env.step(action)
+
+        # lazy sleep to simulate the inference frequency
+        time.sleep(0.5)
+        print(f"{i}:  used time {time.time() - t}")
